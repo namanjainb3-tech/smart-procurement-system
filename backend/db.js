cc157aeb9ff43f947d02d0e7b57cdfcbb355ac01
namanjainb3-tech <namanjainb3@gmail.com>
2026-04-25 02:11:54 +0530

    connected db env variables

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,14 @@
 import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "Delhi@51",
-  database: "smart_procurement",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+
   waitForConnections: true,
-  connectionLimit: 10
+  connectionLimit: 10,
 });
 
 export default db;
